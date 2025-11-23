0a67a434944ac318cf2ec4a20014d62109bea8ef
Nina6z <allahverdiyevanina1618@gmail.com>
2025-11-23 06:53:47 +0400

    Add files via upload

---
+++ myproject6/polls/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'polls/index.html')
+
+def detail(request):
+    return render(request, 'polls/detail.html')
+from django.shortcuts import render, get_object_or_404
+from .models import Question
+
+def index(request):
+    latest_question_list = Question.objects.all()
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
